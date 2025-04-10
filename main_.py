8495c88a0ee7ca2b327b72a3c320d90e004bef50
benrachmut <rachmut.ben@gmail.com>
2025-04-10 16:36:45 -0500

    hi

--- main_.py
+++ main_.py
@@ -435,11 +435,11 @@ if __name__ == '__main__':
     num_opt_clusters_list =[5] #[5]
     mix_percentage = 0.2
     server_split_ratio_list = [0.2]
-    alpha_dichts = [100,10,1,0.5]
+    alpha_dichts = [5]
     epsilons = [1]  # 0.96,0.5,0.75,1,1.25,1.5,1.75,2]
     print("epsilons:", epsilons)
     print(("alpha_dichts", alpha_dichts))
-    algorithm_selection_list = [ AlgorithmSelected.Centralized]#[ AlgorithmSelected.PseudoLabelsClusters,AlgorithmSelected.PseudoLabelsNoServerModel,AlgorithmSelected.FedAvg,AlgorithmSelected.Centralized,AlgorithmSelected.NoFederatedLearning]
+    algorithm_selection_list = [ AlgorithmSelected.PseudoLabelsClusters]#[ AlgorithmSelected.PseudoLabelsClusters,AlgorithmSelected.PseudoLabelsNoServerModel,AlgorithmSelected.FedAvg,AlgorithmSelected.Centralized,AlgorithmSelected.NoFederatedLearning]
 
     # centralized
     nets_types_Centralized_list = [NetsType.S_alex,NetsType.S_vgg]
