aa3985f5419596a16b6d8b34ca18e4b3a558f71f
ben_rachmut <rachmut.ben@gmail.com>
2025-04-17 15:25:17 -0500

    hi

--- Graph_final.py
+++ Graph_final.py
@@ -1,11 +1,24 @@
+from sympy.abc import epsilon
+
 from Graph_global import *
 from config import AlgorithmSelected
 
 def analize_PseudoLabelsClusters():
-    for net_type in merged_dict_dich.keys():
+    ans = {}
+    for net_type in merged_dict_dich_algo.keys():
+        algo_name = algo_names[AlgorithmSelected.PseudoLabelsClusters.name]
         if net_type == NetsType.C_alex_S_vgg.name:
-
+            dict_ = merged_dict_dich_algo[NetsType.C_alex_S_vgg.name]
+            algo_name = algo_name + "_S_vgg"
         if net_type == NetsType.C_alex_S_alex.name:
+            dict_ = merged_dict_dich_algo[NetsType.C_alex_S_alex.name]
+            algo_name = algo_name + "_S_alex"
+        dict_ = dict_["multi_model"]["max"][ClusterTechnique.greedy_elimination_L2.name]["similar_to_cluster"]
+        for epsilon in dict_.keys():
+            algo_name = algo_name + "ε_"+str(epsilon)
+            rd=dict_[epsilon]
+            ans[algo_name] = get_avg_of_entity(rd.server_accuracy_per_client_1_max)
+    return ans
 
 
 def get_data_per_algo(algo):
@@ -27,10 +40,13 @@ if __name__ == '__main__':
     merged_dict = merge_dicts(all_data)
 
     merged_dict = merged_dict["CIFAR100"][25][5][0.2]
+    ans = {}
+
     for dich in [0.5,1,5,10,100]:
+        ans[dich] = {}
         merged_dict_dich = merged_dict[dich]
-        ans = {}
         for algo in merged_dict_dich.keys():
-            dict_return = get_data_per_algo(algo)
+            merged_dict_dich_algo = merged_dict_dich[algo]
+            ans[dich] = get_data_per_algo(algo)
 
     data_for_graph = {}
--- main_.py
+++ main_.py
@@ -435,8 +435,8 @@ if __name__ == '__main__':
     num_opt_clusters_list =[5] #[5]
     mix_percentage = 0.2
     server_split_ratio_list = [0.2]
-    alpha_dichts = [5]
-    epsilons = [1,1.25]  # 0.96,0.5,0.75,1,1.25,1.5,1.75,2]
+    alpha_dichts = [0.5,5]
+    epsilons = [0.75]  # 0.96,0.5,0.75,1,1.25,1.5,1.75,2]
     print("epsilons:", epsilons)
     print(("alpha_dichts", alpha_dichts))
     algorithm_selection_list = [AlgorithmSelected.Centralized]#[ AlgorithmSelected.PseudoLabelsClusters,AlgorithmSelected.PseudoLabelsNoServerModel,AlgorithmSelected.FedAvg,AlgorithmSelected.Centralized,AlgorithmSelected.NoFederatedLearning]
