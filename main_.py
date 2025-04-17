fd6e14d93aa5e105396008a34859a3bf7d4368b3
ben_rachmut <rachmut.ben@gmail.com>
2025-04-17 13:53:06 -0500

    hi

--- main_.py
+++ main_.py
@@ -435,7 +435,7 @@ if __name__ == '__main__':
     num_opt_clusters_list =[5] #[5]
     mix_percentage = 0.2
     server_split_ratio_list = [0.2]
-    alpha_dichts = [5]
+    alpha_dichts = [0.5]
     epsilons = [1,1.25]  # 0.96,0.5,0.75,1,1.25,1.5,1.75,2]
     print("epsilons:", epsilons)
     print(("alpha_dichts", alpha_dichts))
