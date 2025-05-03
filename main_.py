5efa7cfae43ff40e02d1ec6ab5c90920a68aa0fc
ben_rachmut <rachmut.ben@gmail.com>
2025-05-03 07:39:29 -0500

    PseudoLabelsClusters alex

--- main_.py
+++ main_.py
@@ -455,7 +455,7 @@ def run_exp_by_algo():
 
 if __name__ == '__main__':
     print(device)
-    seed_num_list = [5]# 5,6,9[1,2,3,4,5,6,7,8,9]#10:[2,4,5,6,9]#100:[1,2,3,5,7]#[1,2,3,4,5,6,7,8,9]
+    seed_num_list = [6]# 5,6,9[1,2,3,4,5,6,7,8,9]#10:[2,4,5,6,9]#100:[1,2,3,5,7]#[1,2,3,4,5,6,7,8,9]
     data_sets_list = [DataSet.CIFAR10]
     num_clients_list = [25]#[25]
     num_opt_clusters_list =[5] #[5]
