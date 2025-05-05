983c54d49c897a65a4e9a2c2cc299b6143146448
ben_rachmut <rachmut.ben@gmail.com>
2025-05-04 20:35:00 -0500

    PseudoLabelsClusters alex

--- main_.py
+++ main_.py
@@ -462,7 +462,7 @@ if __name__ == '__main__':
     mix_percentage = 0.1
     server_split_ratio_list = [0.2]
     alpha_dichts =[5] #[3,2,100,10,5,1] #[3,2,1,]
-    cluster_additions = [-1]#[-4,-3,-2,-1,0,1,2,3,4] #  # 0.96,0.5,0.75,1,1.25,1.5,1.75,2]
+    cluster_additions = [-2]#[-4,-3,-2,-1,0,1,2,3,4] #  # 0.96,0.5,0.75,1,1.25,1.5,1.75,2]
     print("epsilons:", cluster_additions)
     print(("alpha_dichts", alpha_dichts))
     algorithm_selection_list =[AlgorithmSelected.PseudoLabelsClusters]#, AlgorithmSelected.NoFederatedLearning, AlgorithmSelected.pFedCK]#, AlgorithmSelected.NoFederatedLearning, AlgorithmSelected.pFedCK]#,AlgorithmSelected.PseudoLabelsClusters]
