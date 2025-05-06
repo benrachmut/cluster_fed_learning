5e8e9ea963c4b76e7f8646b780e44841b95d85a2
ben_rachmut <rachmut.ben@gmail.com>
2025-05-06 11:23:01 -0500

    PseudoLabelsClusters alex

--- Graph_algo_final.py
+++ Graph_algo_final.py
@@ -26,20 +26,18 @@ if __name__ == '__main__':
 
     cluster_names = {"Optimal":"CBG",1:"No Clusters"} #Cluster By Group
 
-    all_data = read_all_pkls("data_algo")
+    all_data = read_all_pkls("tttt")#("data_algo")
     merged_dict1 = merge_dicts(all_data)
     #top_what_list = [1,5,10]
 
     what_top_dict = {DataSet.CIFAR100.name:1,DataSet.CIFAR10.name:1,DataSet.TinyImageNet.name:5,DataSet.EMNIST_balanced.name:1}
     #for top_what in top_what_list:
     data_for_graph={}
-    for dich  in [100]:
-        for data_type in [DataSet.CIFAR100.name,DataSet.CIFAR10.name,DataSet.TinyImageNet.name,DataSet.EMNIST_balanced.name]:
+    for dich  in [5]:
+        for data_type in [DataSet.CIFAR100.name]:#[DataSet.CIFAR100.name,DataSet.CIFAR10.name,DataSet.TinyImageNet.name,DataSet.EMNIST_balanced.name]:
             merged_dict = merged_dict1[data_type][25][5][0.2][dich]
             merged_dict = switch_algo_and_seed(merged_dict, dich=dich,data_type=data_type)
             data_for_graph[data_type] = collect_data_per_iteration(merged_dict,what_top_dict[data_type],data_type)
-
-
 
 
         y_label_dict = {DataSet.CIFAR100.name:"Top-1 Accuracy (%)",DataSet.CIFAR10.name:"Top-1 Accuracy (%)",DataSet.TinyImageNet.name:"Top-5 Accuracy (%)",DataSet.EMNIST_balanced.name:"Top-1 Accuracy (%)"}
