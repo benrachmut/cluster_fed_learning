791250aca68c4a37e3352221d091bc8fb31b94e9
ben_rachmut <rachmut.ben@gmail.com>
2025-05-12 20:10:56 -0500

    PseudoLabelsClusters alex

--- main_.py
+++ main_.py
@@ -516,7 +516,7 @@ if __name__ == '__main__':
     cluster_additions = [0]#[-4,-3,-2,-1,0,1,2,3,4] #  # 0.96,0.5,0.75,1,1.25,1.5,1.75,2]
     print("epsilons:", cluster_additions)
     print(("alpha_dichts", alpha_dichts))
-    algorithm_selection_list =[AlgorithmSelected.COMET]
+    algorithm_selection_list =[AlgorithmSelected.PseudoLabelsNoServerModel]
     #AlgorithmSelected.FedAvg,AlgorithmSelected.NoFederatedLearning,AlgorithmSelected.pFedCK
     #AlgorithmSelected.PseudoLabelsClusters
     #AlgorithmSelected.COMET,AlgorithmSelected.PseudoLabelsNoServerModel
