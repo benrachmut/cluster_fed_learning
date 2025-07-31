c021dec5a091e6d2ee5003a5cc809fb7c092ea2a
Tomson Li <tomson.li@wustl.edu>
2025-07-31 12:44:24 -0500

    update

--- functions.py
+++ functions.py
@@ -617,13 +617,22 @@ def get_data_set(is_train ):
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # Use RGB normalization
         ])
     elif dataset == DataSet.IMAGENET:
-        transform = transforms.Compose([
-        transforms.Resize(256),  # Resize to 256 then crop
-        transforms.CenterCrop(224),  # Standard size for ImageNet models
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std =[0.229, 0.224, 0.225])
-        ])
+        if is_train:
+            transform = transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                std =[0.229, 0.224, 0.225])
+            ])
+        else:
+            transform = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                std =[0.229, 0.224, 0.225])
+            ])
     else:
         print("Using RGB transform for dataset")
         transform = transforms.Compose([
