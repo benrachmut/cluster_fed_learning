98718f7feda65ad17e7543edf4724a0c2bb9128f
ben_rachmut <rachmut.ben@gmail.com>
2025-08-14 15:22:27 -0500

    PseudoLabelsClusters alex

--- main_.py
+++ main_.py
@@ -514,7 +514,7 @@ if __name__ == '__main__':
     mix_percentage = 0.1
     server_amount_data_list = [0]#[0,0.25,0.5,0.75,1]
     alpha_dichts =[1] #[3,2,100,10,5,1] #[3,2,1,]
-    cluster_additions = [2]#[-4,-3,-2,-1,0,1,2,3,4] #  # 0.96,0.5,0.75,1,1.25,1.5,1.75,2]
+    cluster_additions = [3]#[-4,-3,-2,-1,0,1,2,3,4] #  # 0.96,0.5,0.75,1,1.25,1.5,1.75,2]
     print("epsilons:", cluster_additions)
     print(("alpha_dichts", alpha_dichts))
     algorithm_selection_list =[AlgorithmSelected.PseudoLabelsClusters]
@@ -523,7 +523,7 @@ if __name__ == '__main__':
     #AlgorithmSelected.COMET,AlgorithmSelected.PseudoLabelsNoServerModel
 
     # parameters for PseudoLabelsClusters
-    nets_types_list_PseudoLabelsClusters  = [NetsType.C_rnd_S_vgg]#[NetsType.C_rnd_S_vgg,NetsType.C_alex_S_alex,NetsType.C_MobileNet_S_alex,NetsType.C_MobileNet_S_vgg,NetsType.C_alex_S_vgg]#,NetsType.C_alex_S_vgg,NetsType.C_alex_S_alex]#,NetsType.C_alex_S_vgg]# ,NetsType.C_alex_S_vgg]#,NetsType.C_alex_S_vgg]#,NetsType.C_alex_S_vgg]
+    nets_types_list_PseudoLabelsClusters  = [NetsType.C_alex_S_vgg]#[NetsType.C_rnd_S_vgg,NetsType.C_alex_S_alex,NetsType.C_MobileNet_S_alex,NetsType.C_MobileNet_S_vgg,NetsType.C_alex_S_vgg]#,NetsType.C_alex_S_vgg,NetsType.C_alex_S_alex]#,NetsType.C_alex_S_vgg]# ,NetsType.C_alex_S_vgg]#,NetsType.C_alex_S_vgg]#,NetsType.C_alex_S_vgg]
     net_cluster_technique_list = [NetClusterTechnique.multi_model]#,NetClusterTechnique.multi_head]
     server_input_tech_list = [ServerInputTech.max]
     cluster_technique_list = [ClusterTechnique.greedy_elimination_L2]#[ClusterTechnique.greedy_elimination_cross_entropy]#[ClusterTechnique.manual_single_iter,ClusterTechnique.manual,ClusterTechnique.kmeans]
