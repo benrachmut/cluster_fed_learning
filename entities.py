24ea0330f4f89c03de5a484d62329410e7d72e5f
ben_rachmut <rachmut.ben@gmail.com>
2025-08-29 09:38:58 -0500

    update

--- entities.py
+++ entities.py
@@ -21,7 +21,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class AlexNet(nn.Module):
