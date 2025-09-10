92e2620016d95d983dad5f31a50eecad0bda688f
ben_rachmut <rachmut.ben@gmail.com>
2025-09-10 16:44:10 -0500

    start rnd nets

--- main_.py
+++ main_.py
@@ -677,7 +677,7 @@ if __name__ == '__main__':
     cluster_additions = [0]
     print("epsilons:", cluster_additions)
     print(("alpha_dichts", alpha_dichts))
-    algorithm_selection_list =[AlgorithmSelected.PseudoLabelsNoServerModel]
+    algorithm_selection_list =[AlgorithmSelected.COMET]
     #AlgorithmSelected.FedAvg,AlgorithmSelected.NoFederatedLearning,AlgorithmSelected.pFedCK,AlgorithmSelected.SCAFFOLD
     #AlgorithmSelected.PseudoLabelsClusters
     #AlgorithmSelected.COMET,AlgorithmSelected.PseudoLabelsNoServerModel
