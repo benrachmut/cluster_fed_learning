7ca1f9c792a251f5f8d257b5016438fa1d789426
ben_rachmut <rachmut.ben@gmail.com>
2025-10-14 09:43:08 -0500

    after going back. start working with json

--- config.py
+++ config.py
@@ -255,7 +255,7 @@ class ExperimentConfig:
             self.server_net_type = NetType.VGG
             self.learning_rate_train_c = 0.0001
             self.learning_rate_fine_tune_c = 0.001
-            self.learning_rate_train_s = 0.0001
+            self.learning_rate_train_s = 0.0005
         if  net_type ==NetsType.C_alex_S_DenseNet:
             self.client_net_type = NetType.ALEXNET
             self.server_net_type = NetType.DenseNetServer
