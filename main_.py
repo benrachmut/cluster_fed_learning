9f54950afcd3177a9385ee2f5a87c7e32fec4e8b
ben_rachmut <rachmut.ben@gmail.com>
2025-10-24 14:44:37 -0500

    after going back. start working with json

--- main_.py
+++ main_.py
@@ -563,7 +563,7 @@ def run_exp_by_algo():
 
 if __name__ == '__main__':
     print(device)
-    seed_num_list = [1,2,3]
+    seed_num_list = [1]
     data_sets_list = [DataSet.CIFAR100]
     num_clients_list = [25]#[25]
     num_opt_clusters_list =[5] #[5]
