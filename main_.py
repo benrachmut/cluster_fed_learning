32bd89611ce54e6cc38a27c34c7be13bb0f43dcf
ben_rachmut <rachmut.ben@gmail.com>
2025-10-28 12:03:06 -0500

    after going back. start working with json

--- main_.py
+++ main_.py
@@ -574,7 +574,7 @@ if __name__ == '__main__':
     server_data_ratios = [1]#[-4,-3,-2,-1,0,1,2,3,4] #  # 0.96,0.5,0.75,1,1.25,1.5,1.75,2]
     print("epsilons:", cluster_additions)
     print(("alpha_dichts", alpha_dichts))
-    algorithm_selection_list =[AlgorithmSelected.MAPL]#[AlgorithmSelected.FedMD,AlgorithmSelected.pFedCK,AlgorithmSelected.COMET,AlgorithmSelected.Ditto, AlgorithmSelected.FedAvg,AlgorithmSelected.FedBABU]
+    algorithm_selection_list =[AlgorithmSelected.FedMD]#[AlgorithmSelected.FedMD,AlgorithmSelected.pFedCK,AlgorithmSelected.COMET,AlgorithmSelected.Ditto, AlgorithmSelected.FedAvg,AlgorithmSelected.FedBABU]
 
     #pFedMe = 11
     #FedBABU = 10
@@ -594,7 +594,7 @@ if __name__ == '__main__':
     #AlgorithmSelected.COMET,AlgorithmSelected.PseudoLabelsNoServerModel
 
     # parameters for PseudoLabelsClusters
-    nets_types_list_PseudoLabelsClusters  = [NetsType.C_squeeze_S_vgg]#,NetsType.C_alex_S_vgg,NetsType.C_alex_S_alex]#,NetsType.C_alex_S_vgg]# ,NetsType.C_alex_S_vgg]#,NetsType.C_alex_S_vgg]#,NetsType.C_alex_S_vgg]
+    nets_types_list_PseudoLabelsClusters  = [NetsType.C_alex_S_alex]#,NetsType.C_alex_S_vgg,NetsType.C_alex_S_alex]#,NetsType.C_alex_S_vgg]# ,NetsType.C_alex_S_vgg]#,NetsType.C_alex_S_vgg]#,NetsType.C_alex_S_vgg]
     homo_models =nets_types_list_PseudoLabelsClusters
 
 
