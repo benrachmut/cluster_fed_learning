af17881cd8a0475b3dd1a90288839347067222f0
ben_rachmut <rachmut.ben@gmail.com>
2025-11-20 11:07:28 -0600

    before changing top5

--- main_.py
+++ main_.py
@@ -632,7 +632,7 @@ def run_exp_by_algo():
 
 if __name__ == '__main__':
     print(device)
-    seed_num_list = [1]
+    seed_num_list = [2]
     data_sets_list =[DataSet.CIFAR100]#,DataSet.ImageNetR,DataSet.TinyImageNet,DataSet.EMNIST_balanced,DataSet.CIFAR10]#[DataSet.CIFAR100]
     num_clients_list = [25]#[100,500]#[25]
     num_opt_clusters_list =[5] #[5]
