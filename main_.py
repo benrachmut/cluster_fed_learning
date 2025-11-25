9151a1f0a5eddd2b594e11f2d9867f267e98b525
ben_rachmut <rachmut.ben@gmail.com>
2025-11-25 11:11:29 -0600

    before changing top5

--- main_.py
+++ main_.py
@@ -674,7 +674,7 @@ def run_FedCT():
 
 if __name__ == '__main__':
     print(device)
-    seed_num_list = [1]
+    seed_num_list = [2]
     data_sets_list =[DataSet.CIFAR100]#,DataSet.ImageNetR,DataSet.TinyImageNet,DataSet.EMNIST_balanced,DataSet.CIFAR10]#[DataSet.CIFAR100]
     num_clients_list = [25]#[100,500]#[25]
     num_opt_clusters_list =[5] #[5]
