956ae756894bb71acc5cd594ece1b16776e03802
ben_rachmut <rachmut.ben@gmail.com>
2025-11-28 20:00:25 -0600

    before changing top5

--- main_.py
+++ main_.py
@@ -674,18 +674,18 @@ def run_FedCT():
 
 if __name__ == '__main__':
     print(device)
-    seed_num_list = [3]
-    data_sets_list =[DataSet.CIFAR100]#,DataSet.ImageNetR,DataSet.TinyImageNet,DataSet.EMNIST_balanced,DataSet.CIFAR10]#[DataSet.CIFAR100]
+    seed_num_list = [1,2,3]
+    data_sets_list =[DataSet.ImageNetR,DataSet.TinyImageNet,DataSet.EMNIST_balanced]#[DataSet.CIFAR100]
     num_clients_list = [25]#[100,500]#[25]
     num_opt_clusters_list =[5] #[5]
     mix_percentage = 0.1
     server_split_ratio_list = [0.2]
-    alpha_dichts =[5] #[3,2,100,10,5,1] #[3,2,1,]
+    alpha_dichts =[100] #[3,2,100,10,5,1] #[3,2,1,]
     cluster_additions = [0]
     server_data_ratios = [1]#[-4,-3,-2,-1,0,1,2,3,4] #  # 0.96,0.5,0.75,1,1.25,1.5,1.75,2]
     print("epsilons:", cluster_additions)
     print(("alpha_dichts", alpha_dichts))
-    algorithm_selection_list = [AlgorithmSelected.FedCT]
+    algorithm_selection_list = [AlgorithmSelected.MAPL]
     #[AlgorithmSelected.COMET, AlgorithmSelected.Ditto, AlgorithmSelected.FedBABU]
         #[AlgorithmSelected.FedMD,AlgorithmSelected.pFedCK,AlgorithmSelected.FedAvg]
 
@@ -714,7 +714,7 @@ if __name__ == '__main__':
     #nets_types_list_PseudoLabelsClusters  = [NetsType.C_AlexSqueeze_S_alex,NetsType.C_AlexMobile_S_alex]
     #[NetsType.C_Mobile_S_alex, NetsType.C_ResNet_S_alex, NetsType.C_squeeze_S_alex]
     #NetsType.C_rnd_S_alex C_rndStrong_S_alex
-    nets_types_list_PseudoLabelsClusters  = [NetsType.C_rnd_S_alex ]
+    nets_types_list_PseudoLabelsClusters  = [NetsType.C_rndWeak_S_VGG]
     homo_models =nets_types_list_PseudoLabelsClusters
 
 
