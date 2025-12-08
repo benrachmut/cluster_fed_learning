245cc867d54ab9d7f96d3379a8e89cf9a34aff1e
ben_rachmut <rachmut.ben@gmail.com>
2025-12-07 20:32:01 -0600

    before changing top5

--- main_.py
+++ main_.py
@@ -682,13 +682,13 @@ def run_FedCT():
 
 if __name__ == '__main__':
     print(device)
-    seed_num_list = [3]
+    seed_num_list = [1,2]
     data_sets_list =[DataSet.CIFAR10]
     num_clients_list = [25]#[100,500]#[25]
     num_opt_clusters_list =[5] #[5]
     mix_percentage = 0.1
     server_split_ratio_list = [0.2]
-    alpha_dichts =[1,5] #[3,2,100,10,5,1] #[3,2,1,]
+    alpha_dichts =[1] #[3,2,100,10,5,1] #[3,2,1,]
     server_data_ratios = [1]#[-4,-3,-2,-1,0,1,2,3,4] #  # 0.96,0.5,0.75,1,1.25,1.5,1.75,2]
 
     print(("alpha_dichts", alpha_dichts))
