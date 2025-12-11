b3d081917d0afd8389847fe06a3e65309ddd81e2
ben_rachmut <rachmut.ben@gmail.com>
2025-12-11 10:32:54 -0600

    before changing top5

--- main_.py
+++ main_.py
@@ -738,8 +738,8 @@ if __name__ == '__main__':
     # C_rndStrong_S_VGG = 11
     # C_rndWeak_S_VGG = 13
 
-    #alex, mobile, res, squeeze
-    nets_types_list_PseudoLabelsClusters  = [NetsType.C_squeeze_S_alex]#[NetsType.C_AlexMobile_S_vgg, NetsType.C_ResNetMobile_S_vgg]#[NetsType.C_rnd_S_VGG, NetsType.C_rndStrong_S_VGG,NetsType.C_ResNetSqueeze_S_vgg]#[NetsType.C_Mobile_S_alex,NetsType.C_alex_S_alex, NetsType.C_squeeze_S_alex,NetsType.C_ResNet_S_alex]
+    #res, squeeze
+    nets_types_list_PseudoLabelsClusters  = [NetsType.C_ResNet_S_alex]#[NetsType.C_AlexMobile_S_vgg, NetsType.C_ResNetMobile_S_vgg]#[NetsType.C_rnd_S_VGG, NetsType.C_rndStrong_S_VGG,NetsType.C_ResNetSqueeze_S_vgg]#[NetsType.C_Mobile_S_alex,NetsType.C_alex_S_alex, NetsType.C_squeeze_S_alex,NetsType.C_ResNet_S_alex]
     homo_models =nets_types_list_PseudoLabelsClusters
 
 
